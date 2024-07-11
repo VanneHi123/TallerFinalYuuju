4e1e72d5e379bf854a544d6a74fd749f4a24f634
Melissabeltran <desarrollo@academicos.cesde.edu.co>
2024-07-11 17:23:01 -0500

    Sigue

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,6 +1,7 @@
 function cambiarCaja(){
 let boton=document.getElementById("botonChat")
 let cajaChat=document.getElementById("cajaChat")
+//cajaChat.style.display=""
 
 if(cajaChat.style.display=="none" || cajaChat.style.display==""){
     cajaChat.style.display="block"
